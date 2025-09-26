5f40b6d95ee45a14ea0e8f71f71a6300771939ea
VITORIA KAORI KURIYAMA <082220005@faculdade.cefsa.edu.br>
2025-09-25 22:43:38 -0300

    aaa

--- Styles/CreatorChooseScreenStyles.js
+++ Styles/CreatorChooseScreenStyles.js
@@ -4,37 +4,66 @@ import Colors from "./Colors";
 
 const styles = StyleSheet.create({
     container: {
-        backgroundColor: Colors.background,
-        alignItems: "center",
-        justifyContent: "center",
+        ...BaseStyles.container,
         padding: 20,
     },
     title: {
-        fontSize: 30,
+        fontSize: 24,
         fontWeight: "bold",
         color: Colors.secondary,
         alignSelf: "center",
-        marginTop: 70,
+        marginBottom: 20,
         fontFamily: "monospace",
-        textTransform: 'uppercase',
+        textTransform: "uppercase",
     },
     text: {
         fontSize: 18,
         color: Colors.textPrimary,
     },
-    button: {
-        backgroundColor: 'rgba(72, 87, 145, 0.5)',
-        padding: 10,
-        borderRadius: 16,
-        marginVertical: 8,
-        height: 100,
-        width: 150,
-        justifyContent: "center",
-        alignItems: "center",
+    textDelete: {
+        color: "#cc0000",
+        fontSize: 13,
+        textTransform: "uppercase",
+        fontWeight: "bold",
+    },
+    textRename: {
+        color: "#000766ff",
+        fontSize: 13,
+        textTransform: "uppercase",
+        
     },
     buttonText: {
-        color: "blue",
+        color: "#5b7a9eff",
         fontSize: 18,
+    },
+    button: {
+        flex: 1,
+        paddingVertical: 10,
+        borderRadius: 8,
+        alignItems: "center",
+        marginHorizontal: 5,
+    },
+    buttonDelete: {
+        backgroundColor: "#f7c1c1ff",
+    },
+    buttonRename: {
+        backgroundColor: "#ababebff",
+    },
+    themeItem: {
+        marginBottom: 20,
+        padding: 15,
+        borderWidth: 1,
+        borderColor: "#e4e4e4ff",
+        borderRadius: 10,
+        backgroundColor: "#e9e9e9ff",
+    },
+    themeName: {
+        fontSize: 18,
+        marginBottom: 10,
+    },
+    buttonGroup: {
+        flexDirection: "row",
+        justifyContent: "space-between",
     },
 });
 
